6b50e44f269c1fdf58c6e872b56e1a808bf0bfd2
damasake3 <171648817+damasake3@users.noreply.github.com>
2025-01-15 01:33:18 +0800

    Finished Chapter 3
    If have time, change JSON to MSSQL as db

---
+++ Chapter3/MyBlog/Data.Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Comment
+    {
+        public string? Id { get; set; }
+        public required string BlogPostId { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
