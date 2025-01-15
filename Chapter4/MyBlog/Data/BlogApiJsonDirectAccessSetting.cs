59a2c73783f36751740b13f8170479eb0406fad4
damasake3 <171648817+damasake3@users.noreply.github.com>
2025-01-15 16:12:09 +0800

    Finished Chapter 4
    Need more practice on correctly building Blazor components

---
+++ Chapter4/MyBlog/Data/BlogApiJsonDirectAccessSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BlogApiJsonDirectAccessSetting
+    {
+        public string BlogPostsFolder { get; set; } = string.Empty;
+        public string CategoriesFolder { get; set; } = string.Empty;
+        public string TagsFolder { get; set; } = string.Empty;
+        public string CommentsFolder { get; set; } = string.Empty;
+        public string DataPath { get; set; } = string.Empty;
+
+    }
+}
